b696e1e8844fed7ca7cb67d3203e19fe98e0d814
Angelo James Arriola <angelojames.arriola@gmail.com>
2018-08-29 13:43:14 -0700

    created form for user login

--- script.js
+++ script.js
@@ -1,118 +1,115 @@
-var cvs = document.getElementById("canvas");
-var ctx = cvs.getContext("2d");
+// ============================================================
 
-var bird = new Image();
-var bg = new Image();
-var fg = new Image();
-var pipeNorth = new Image();
-var pipeSouth = new Image();
-var trollBg = document.querySelector('#bg-img');
+// MODAL
+
+// ============================================================
+
+// ============================================================
+
+// FONZY BIRD GAME
+
+// ============================================================
+// var cvs = document.getElementById("canvas");
+// var ctx = cvs.getContext("2d");
+
+// var bird = new Image();
+// var bg = new Image();
+// var fg = new Image();
+// var pipeNorth = new Image();
+// var pipeSouth = new Image();
+// var trollBg = document.querySelector('#bg-img');
 
 
-var fly = new Audio();
-var scor = new Audio();
-var trollSound = new Audio();
+// var fly = new Audio();
+// var scor = new Audio();
+// var trollSound = new Audio();
 
-fly.src = "assets/images/bark.mp3";
-scor.src = "";
-trollSound = "";
-//Find images and pluggin in src to load images onto game
+// fly.src = "assets/images/bark.mp3";
+// scor.src = "";
+// trollSound.src = "";
+// //Find images and pluggin in src to load images onto game
 
-bird.src = "assets/images/fonzy2.0.png";
-bg.src = "assets/images/bg.png";
-fg.src = "assets/images/fg.png";
-pipeNorth.src = "assets/images/pipeNorth.png";
-pipeSouth.src = "assets/images/pipeSouth.png";
+// bird.src = "assets/images/fonzy2.0.png";
+// bg.src = "assets/images/bg.png";
+// fg.src = "assets/images/fg.png";
+// pipeNorth.src = "assets/images/pipeNorth.png";
+// pipeSouth.src = "assets/images/pipeSouth.png";
 
-//Creats the gap after the pipe and before the southpipe starts
-var gap = 65;
-var constant = pipeNorth.height+gap;
+// //Creats the gap after the pipe and before the southpipe starts
+// var gap = 80;
+// var constant = pipeNorth.height+gap;
 
-//gives the starting position for the player
-var bx = 10;
-var by = 150;
+// //gives the starting position for the player
+// var bx = 10;
+// var by = 150;
 
-//creates gravity so that the player sprite moves down the y axis
-var gravity = 2.5;
-//a score counter to increment by one as the player passes through the pipes
-var score = 0;
-//adding an eventlistener to wait for the user to pressdown on a key
-document.addEventListener("keydown",moveUp);
+// //creates gravity so that the player sprite moves down the y axis
+// var gravity = 2.5;
+// //a score counter to increment by one as the player passes through the pipes
+// var score = 0;
+// //adding an eventlistener to wait for the user to pressdown on a key
+// document.addEventListener("keydown",moveUp);
 
-document.addEventListener("keyUp", stop);
+// function moveUp() {
+//     var key = event.keyCode;
+//     // var 
+//     //specifically waiting for the user to press down on the keycode "32" which is the space bar
+//     // if (key === 32) {
+//         by -= 35;
+//     // }    
+// }
 
-function moveUp() {
-    var key = event.keyCode;
-    // var 
-    //specifically waiting for the user to press down on the keycode "32" which is the space bar
-    if (key === 32) {
-        by -= 40;
-        fly.play();
-    }    
-}
+// var pipe = [];
 
-function stop() {
-    var key = event.keyCode;
+// pipe[0] = {
+//     x : cvs.width,
+//     y : 0
+// }
 
-    if (key===32) {
-        fly.stop();
-    }
-}
-//
-var pipe = [];
+// function draw() {
+//     ctx.drawImage(bg,0,0);
 
-pipe[0] = {
-    x : cvs.width,
-    y : 0
-}
+//     for (var i = 0; i < pipe.length; i++) {
+//         // score++;
+//         ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);
+//         ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y+constant);
 
-function draw() {
-    ctx.drawImage(bg,0,0);
+//         pipe[i].x-=1;
 
-    for (var i = 0; i < pipe.length; i++) {
-        
-        ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);
-        ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y+constant);
+//         if (pipe[i].x === 125) {
+//             pipe.push({
+//                 x: cvs.width,
+//                 y: Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height
+//             });
+//         }
+//         if (bx + bird.width >= pipe[i].x && bx <= pipe[i].x + pipeNorth.width
+//         && (by <= pipe[i].y + pipeNorth.height || by+bird.height >= pipe[i].y+constant)
+//         || by + bird.height >= cvs.height - fg.height)
+//          {  
+//             // trollBg.style.display = 'block';
+//             // add flag asking if game is over!!!!!!
+//             // confirm("GAME OVER!! Your score was " + score );
+//             location.reload();
+//         }
+//         if (pipe[i].x == 10) {
+//             score++;
+//         }
+//     }
 
-        pipe[i].x--;
+//     // ctx.drawImage(pipeNorth,150,0);
+//     // ctx.drawImage(pipeSouth,150,0+constant);
 
-        if (pipe[i].x === 125) {
-            pipe.push({
-                x: cvs.width,
-                y: Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height
-            });
-        }
-        if (bx + bird.width >= pipe[i].x && bx <= pipe[i].x + pipeNorth.width
-        && (by <= pipe[i].y + pipeNorth.height || by+bird.height >= pipe[i].y+constant)
-        || by + bird.height >= cvs.height - fg.height)
-         {  
-            // trollBg.style.display = 'block';
-            // add flag asking if game is over!!!!!!
-            if (confirm("GAME OVER!! Your score was " + score )){
-                
-            }
-            location.reload();
-        }
-        if (pipe[i].x == 5) {
-            score++;
-        }
-    }
+//     ctx.drawImage(fg,0,cvs.height - fg.height);
 
-    // ctx.drawImage(pipeNorth,150,0);
-    // ctx.drawImage(pipeSouth,150,0+constant);
+//     ctx.drawImage(bird,bx,by);
 
-    ctx.drawImage(fg,0,cvs.height - fg.height);
+//     by += gravity;
 
-    ctx.drawImage(bird,bx,by);
-
-    by += gravity;
-
-    ctx.fillStyle = "#000";
-    ctx.font = "40px Comic Sans MS";
-    ctx.fillText("Score : " + score, 10,cvs.height-20);
+//     ctx.fillStyle = "#000";
+//     ctx.font = "40px Comic Sans MS";
+//     ctx.fillText("Score : " + score, 10,cvs.height-20);
     
-    requestAnimationFrame(draw);
-}
+//     requestAnimationFrame(draw);
+// }
 
-draw();
-
+// draw();
